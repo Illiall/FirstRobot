82e825e084b9f6ae1e132537a425949616842588
Eldar398 <superwatermelony@gmail.com>
2024-12-18 16:29:05 -0600

    Constant File ADDED

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,4 +5,22 @@
 package frc.robot;
 
 /** Add your docs here. */
-public class Constants {}
+public class Constants {
+    //Drive train Constants
+    //Drive train  Motors
+    //Drivetrain Ids
+    public static final int right_m1_Id = 0;
+    public static final int right_m2_Id = 1;
+    public static final int left_m1_Id = 2;
+    public static final int left_m2_Id = 3;
+
+    //Pivot Constants
+    //Pivot Motor Id
+    public static final int pivot_motor_Id = 4;
+    //Pivot Speed
+    public static final double pivot_speed = 0.5;
+
+
+
+
+}
